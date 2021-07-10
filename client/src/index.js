cbdf88897b3733919c188fd49df42772adc8abc3
Ishan Kumar <itsik159@gmail.com>
2021-07-10 12:22:46 +0530

    Added boilerplate for MERN Stack

---
+++ client/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { ToastProvider } from "react-toast-notifications";
+import { BrowserRouter } from "react-router-dom";
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_CALL;
+ReactDOM.render(
+  <React.StrictMode>
+  <BrowserRouter>
+  <ToastProvider>
+    <App />
+  </ToastProvider>
+  </BrowserRouter>
+</React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express()
+const mongoose = require('mongoose');
+require('dotenv').config({path: './.env'});
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json());
+
+
+//connect to mongo db atlas
+mongoose.connect(process.env.MONGO_URL,
+{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false },
+).then(() => {
+    console.log('DB CONNECTED');
+  })
+  .catch(console.log('DB NOT CONNECTED'));
+
+
+//check if backend responding
+app.get('/' , (req , res) => {
+    return res.status(201).send("Backend Check!")
+})
+//backend running on PORT  7000
+const port = process.env.PORT || 7000;
+app.listen( port , () => { console.log("server up at " +port);})
